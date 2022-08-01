80e1353666a90c89316bef7feab62400caedb197
Alex Myachin <myachin_aleksei@mail.ru>
2022-08-01 23:18:36 +0600

    Multiple Conditions.

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if (character < 65 || character > 122){
+            System.out.println("wrong alphabet!");
+        }else if(character == 91 || character == 92 ||character == 93 ||character == 94 ||character == 95 ||
+                character == 96){
+            System.out.println("wrong alphabet!");
+        }else if(character == 65 || character == 69 || character == 73 || character == 79 || character == 85 ||
+                character == 89 || character == 97 || character == 101 || character == 105 || character == 111 ||
+                character == 117 || character == 121){
+            System.out.println("Vowel");
+        }else{
+            System.out.println("Consonant");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+        if (symbol == 91 || symbol == 92 ||symbol == 93 ||symbol == 94 ||symbol == 95 || symbol == 96) {
+            System.out.println("Non English");
+        }else if (symbol > 65 && symbol < 122){
+            System.out.println("English");
+        }else{
+            System.out.println("Non English");
+        }
     }
 }
